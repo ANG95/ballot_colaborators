74e0ab0a6b118666f170f3798ccd7b50b3946855
German Apaza <german.apaza.n@gmail.com>
2024-11-29 12:52:15 -0500

    feature: implementation modal collaborator details

--- app/pages/administrator/page.tsx
+++ app/pages/administrator/page.tsx
@@ -1,20 +1,21 @@
 "use client";
 import DataTable from "@/components/common/dataTable/dataTable";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useGetUsersRol } from "./hooks/useGetUsersRol";
-import { Button } from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { CollaboratorType } from "@/types/collaborator";
 
 const Administrator = () => {
   const { data } = useGetUsersRol();
 
-  const [createUpdatePatientModal, setCreateUpdatePatientModal] = useState(false);
+  const [collaboratorDetailModal, setCollaboratorDetailModal] = useState(false);
   const [numPages, setNumPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [rowsPerPage] = useState(10);
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredData, setFilteredData] = useState(data);
+  const collaboratorSelector = useRef({})
 
-  console.log(createUpdatePatientModal )
   useEffect(() => {
     if (data && filteredData) {
       setNumPages(Math.ceil(filteredData.length / rowsPerPage));
@@ -36,61 +37,86 @@ const Administrator = () => {
   };
 
   const updateCreatePatient = (e) => {
-    const valuesParsed =  JSON.parse(e.target.value)
+    const valuesParsed = JSON.parse(e.target.value)
     console.log('ejecutando el click', valuesParsed)
   };
 
-  const handleViewTickets = (e) => {
-    console.log('ejecutando el click', e.target.value)
+  const handleUserDetails = (e) => {
+    const dataCollaboratorParsed = JSON.parse(e.target.value)
+    console.log('ejecutando el click',dataCollaboratorParsed)
+
+    collaboratorSelector.current = dataCollaboratorParsed;
     // Lógica para crear o actualizar paciente
+    setCollaboratorDetailModal(true)
   };
 
   const handleSearchInTable = (value: string) => {
     setSearchTerm(value);
     setCurrentPage(1);
   };
-
+  const collaboratorSelected: CollaboratorType | any = collaboratorSelector.current
   return (
-    <div className="flex flex-col h-full">
-      <div>
-        <h1 className="text-2xl mb-4">Colaboradores</h1>
+    <>
+      <div className="flex flex-col h-full">
+        <div>
+          <h1 className="text-2xl mb-4">Colaboradores</h1>
+        </div>
+        <DataTable
+          handleSearch={({ target: { value } }: any) => handleSearchInTable(value)}
+          addNewPress={() => { }}
+          columns={[
+            { name: 'Foto', value: 'picture', type: 'img' },
+            { name: 'NOMBRES', value: 'name', },
+            { name: 'APELLIDOS', value: 'family_name' },
+            { name: '', value: 'isChildren' },
+          ]}
+          rows={filteredData && filteredData.slice((currentPage - 1) * rowsPerPage, currentPage * rowsPerPage)}
+          rightComponents={
+            <div className="flex">
+              <Button
+                className="edit-button"
+                onClick={(e) => handleUserDetails(e)}
+                color="success"
+                size="sm"
+              >
+                Detalles
+              </Button>
+              <div className="ml-1" />
+              <Button
+                className="edit-button"
+                onClick={(e) => updateCreatePatient(e)}
+                color="primary"
+                size="sm"
+              >
+                Cargar Boletas
+              </Button>
+            </div>
+          }
+          totalPages={numPages}
+          currentPage={currentPage}
+          onChangePage={(page) => ReqDataAPI(page)}
+        />
       </div>
-      <DataTable
-        handleSearch={({ target: { value } }: any) => handleSearchInTable(value)}
-        addNewPress={() => setCreateUpdatePatientModal(true)}
-        columns={[
-          { name: 'Foto', value: 'picture', type: 'img' },
-          { name: 'NOMBRES', value: 'name', },
-          { name: 'APELLIDOS', value: 'family_name' },
-          { name: '', value: 'isChildren' },
-        ]}
-        rows={filteredData && filteredData.slice((currentPage - 1) * rowsPerPage, currentPage * rowsPerPage)}
-        rightComponents={
-          <div className="flex">
-            <Button
-              className="edit-button"
-              onClick={(e) => handleViewTickets(e)}
-              color="success"
-              size="sm"
-            >
-              Detalles
-            </Button> 
-            <div className="ml-1"/>
-            <Button
-              className="edit-button"
-              onClick={(e) => updateCreatePatient(e)}
-              color="primary"
-              size="sm"
-            >
-              Cargar Boletas
-            </Button>
-          </div>
-        }
-        totalPages={numPages}
-        currentPage={currentPage}
-        onChangePage={(page) => ReqDataAPI(page)}
-      />
-    </div>
+      <Modal isOpen={collaboratorDetailModal} toggle={() => setCollaboratorDetailModal(false)}>
+        <ModalHeader toggle={() => setCollaboratorDetailModal(false)}>Detalles del colaborador</ModalHeader>
+        <ModalBody>
+            <img src={collaboratorSelected.picture} alt={collaboratorSelected.picture} />
+            <hr />
+            <div>
+              {collaboratorSelected.given_name}
+            </div>
+            <hr />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={() => setCollaboratorDetailModal(false)}>
+            Do Something
+          </Button>{' '}
+          <Button color="secondary" onClick={() => setCollaboratorDetailModal(false)}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
   );
 };
 
---
+++ src/types/collaborator.ts
@@ -0,0 +1,13 @@
+export interface CollaboratorType {
+  created_at: string;
+  descripcion: string | null;
+  email: string;
+  family_name: string;
+  given_name: string;
+  id: number;
+  name: string;
+  picture: string | null;
+  rol_id: number;
+  rol_nombre: string;
+  updated_at: string;
+}
