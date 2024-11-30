b4396fce44ae6cf220ba289b925240ebb07f9e63
German Apaza <german.apaza.n@gmail.com>
2024-11-29 20:24:41 -0500

    feat: add file implementation

--- app/pages/invoice/hooks/useAddInvoices.ts
+++ app/pages/invoice/hooks/useAddInvoices.ts
@@ -4,35 +4,41 @@ import { toast } from "react-toastify";
 
 export const useAddInvoices = () => {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   const handleInvoiceInsert = async (invoiceFiles: File[], userId: string, period: string) => {
     setLoading(true);
     setError(null);
-  
+
     const formData = new FormData();
     invoiceFiles.forEach((file) => {
       formData.append('invoiceFile', file);
     });
     formData.append("userId", userId);
     formData.append("period", period);
-  
+
     try {
       const response = await apiClient.post("/api/invoice", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
-      setData(response.data); 
+      setData(response.data);
+      setLoading(false);
       toast.success("¡Datos guardados correctamente!")
     } catch (error: any) {
-      setError("Error al obtener colaboradores: " + error.message);
-      toast.error("¡Ups!, ocurrió un error al guardar los datos, por favor intente nuevamente")
+      if (error.response?.data?.error) {
+        toast.error("¡Ups, Error!, " + error.response?.data?.error)
+      } else {
+        setError("Error al obtener colaboradores: " + error.message);
+        toast.error("¡Ups!, ocurrió un error al guardar los datos, por favor intente nuevamente")
+      }
+      setLoading(false);
     } finally {
       setLoading(false);
     }
   };
-  
+
   return { insertInvoiceResult: data, insertInvoiceLoading: loading, insertInvoiceError: error, handleInvoiceInsert };
 };
